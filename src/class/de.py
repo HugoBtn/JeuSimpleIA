8d27a74084c6cc7eb9855b266dfd7cf6cd3c346d
hugob <hugobetton2004@gmail.com>
2026-01-16 17:48:27 +0100

    de

--- src/class/de.py
+++ src/class/de.py
@@ -1,6 +1,25 @@
-class De:
-    def __init__(self, value):
-        self.value = value
+import random
 
-    def display(self):
-        return f"De value is: {self.value}"
+
+class De:
+    def __init__(self):
+        self.__value = random.randint(1, 6)
+    
+    def get_value(self):
+        return self.__value.copy()
+    
+    def lancer(self):
+        self.__value = random.randint(1, 6)
+
+    def __str__(self):
+        return f"La valeur du dé est {str(self.__value)}"
+
+
+if __name__ == "__main__":
+    De1 = De()
+    De2 = De()
+    print(De1)
+    print(De2)
+    De1.lancer()
+    print(De1)
+    print(De2)
